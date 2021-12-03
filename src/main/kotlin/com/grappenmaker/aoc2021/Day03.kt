021b626d05771b4dee4c1794f18bcd565e5170bd
770grappenmaker <770grappenmaker@gmail.com>
2021-12-03 22:42:00 +0100

    Day 3 - Improvements

--- src/main/kotlin/com/grappenmaker/aoc2021/Day03.kt
+++ src/main/kotlin/com/grappenmaker/aoc2021/Day03.kt
@@ -1,18 +1,16 @@
 package com.grappenmaker.aoc2021
 
-val negate = { num: Int -> if (num >= 1) 0 else 1 }
+import kotlin.math.pow
 
 fun solveDay3() {
     // Part one
     val lines = getInputLines(3)
-    val indices = lines.first().indices
-    val numbers = lines.map { it.toCharArray().map { c -> if (c == '1') 1 else 0 } }
+    val width = lines.first().length
+    val numbers = lines.map { it.toInt(2) }
 
-    val mostCommon = indices.map { getMostCommon(it, numbers) }
-    val leastCommon = mostCommon.map(negate)
-
-    val gamma = mostCommon.foldBits()
-    val epsilon = leastCommon.foldBits()
+    val gamma = (0 until width)
+        .reduceIndexed { idx, acc, v -> acc or (getMostCommon(width, v, numbers) shl width - idx - 1) }
+    val epsilon = gamma.inv() and 2.0.pow(width).toInt() - 1
 
     println("Part one: ${gamma to epsilon} -> ${gamma * epsilon}")
 
@@ -20,26 +18,25 @@ fun solveDay3() {
     val findRating = { doNegate: Boolean ->
         var result = numbers
 
-        for (i in indices) {
-            val toCheck = getMostCommon(i, result)
-            result = result.filter { it[i] == if (doNegate) negate(toCheck) else toCheck }
+        for (i in 0 until width) {
+            val toCheck = getMostCommon(width, i, result)
+            result = result.filter { it.getBit(width - i - 1) == if (doNegate) toCheck.inv() and 1 else toCheck }
 
             if (result.size == 1) break
         }
 
-        result.first().foldBits()
+        result.first()
     }
 
     val oxygenRating = findRating(false)
-    val co2SrubRating = findRating(true)
+    val co2ScrubRating = findRating(true)
 
-    println("Part two: ${oxygenRating to co2SrubRating} -> ${oxygenRating * co2SrubRating}")
+    println("Part two: ${oxygenRating to co2ScrubRating} -> ${oxygenRating * co2ScrubRating}")
 }
 
-fun getMostCommon(idx: Int, values: List<List<Int>>): Int {
-    val sum = values.sumOf { b -> (if (b[idx] == 1) 1 else -1).toInt() }
+fun Int.getBit(index: Int) = this shr index and 1
+
+fun getMostCommon(width: Int, idx: Int, values: List<Int>): Int {
+    val sum = values.sumOf { b -> (if (b.getBit(width - idx - 1) == 1) 1 else -1).toInt() }
     return if (sum >= 0) 1 else 0
 }
-
-fun Iterable<Int>.foldBits() =
-    foldIndexed(0) { idx: Int, acc: Int, v: Int -> acc or (v shl count() - idx - 1) }
