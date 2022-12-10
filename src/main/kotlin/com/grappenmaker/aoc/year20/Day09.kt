2e8a92b88b2ce93cfca6a37e7ef208ce9c71e8bf
770grappenmaker <770grappenmaker@gmail.com>
2022-12-10 22:48:54 +0100

    Day 9 - 2020

---
+++ src/main/kotlin/com/grappenmaker/aoc/year20/Day09.kt
@@ -0,0 +1,20 @@
+package com.grappenmaker.aoc.year20
+
+import com.grappenmaker.aoc.PuzzleSet
+import com.grappenmaker.aoc.year22.permPairsExclusive
+
+const val preambleSize = 25
+
+fun PuzzleSet.day9() = puzzle(9) {
+    val nums = inputLines.map(String::toLong)
+    val other = nums.drop(preambleSize)
+    val invalid = other.filterIndexed { idx, v ->
+        v !in nums.subList(idx, idx + preambleSize).permPairsExclusive().map { (a, b) -> a + b }
+    }.first()
+
+    partOne = invalid.s()
+    val range = generateSequence(2) { it + 1 }.firstNotNullOf { nums.windowed(it).find { w -> w.sum() == invalid } }
+        .sorted()
+
+    partTwo = (range.first() + range.last()).s()
+}
