a554f6b086868d258555a5a7c21ff912d4dc461b
770grappenmaker <770grappenmaker@gmail.com>
2022-12-14 11:37:03 +0100

    Day 14 - 2022

--- src/main/kotlin/com/grappenmaker/aoc/year22/Day14.kt
+++ src/main/kotlin/com/grappenmaker/aoc/year22/Day14.kt
@@ -1,7 +1,6 @@
 package com.grappenmaker.aoc.year22
 
 import com.grappenmaker.aoc.PuzzleSet
-import com.grappenmaker.aoc.year22.BlockType.*
 import com.grappenmaker.aoc.year22.Direction.*
 
 fun PuzzleSet.day14() = puzzle {
@@ -10,45 +9,38 @@ fun PuzzleSet.day14() = puzzle {
             .windowed(2).map { (a, b) -> a..b }.connect()
     }
 
-    val initial = lines.asBooleanGrid().mapElements { if (it) WALL else EMPTY }
     val sandLocation = Point(500, 0)
     val directions = listOf(DOWN.toPoint(), DOWN + LEFT, DOWN + RIGHT)
 
-    fun MutableGrid<BlockType>.sim(): Int {
+    fun HashSet<Point>.sim(partTwo: Boolean): Int {
+        val p2h = if (partTwo) lines.maxY() + 2 else lines.maxY()
+        val yRange = 0..p2h
+        var sand = 0
+
+        fun isFilled(p: Point) = if (partTwo && p.y == p2h) true else p in this
+
         outer@ while (true) {
             var curr = sandLocation
             while (true) {
-                val poss = directions.map { curr + it }.filter { it in this }
+                val poss = directions.map { curr + it }.filter { it.y in yRange }
                 if (poss.isEmpty()) break@outer
 
-                val new = poss.firstOrNull { this[it] == EMPTY }
+                val new = poss.firstOrNull { !isFilled(it) }
                 if (new != null) {
                     curr = new
                     continue
                 }
 
-                if (this[curr] == SAND) break@outer
-                this[curr] = SAND
+                if (isFilled(curr)) break@outer
+                this += curr
+                sand++
                 break
             }
         }
 
-        return count { it == SAND }
+        return sand
     }
 
-    partOne = initial.asMutableGrid().sim().s()
-
-    val newHeight = initial.height + 2
-    partTwo = initial.extend(width = initial.width * 2, height = newHeight)
-    { if (it.y == newHeight - 1) WALL else EMPTY }.asMutableGrid().sim().s()
-}
-
-enum class BlockType {
-    EMPTY, SAND, WALL;
-
-    override fun toString() = when (this) {
-        EMPTY -> "."
-        SAND -> "o"
-        WALL -> "#"
-    }
+    partOne = lines.toHashSet().sim(false).s()
+    partTwo = lines.toHashSet().sim(true).s()
 }
