1cf3a9414d7aa6c1ef5a94b4d24d7de78a8138e3
770grappenmaker <770grappenmaker@gmail.com>
2022-12-16 22:15:48 +0100

    Day 20 - 2015

---
+++ src/main/kotlin/com/grappenmaker/aoc/year15/Day20.kt
@@ -0,0 +1,18 @@
+package com.grappenmaker.aoc.year15
+
+import com.grappenmaker.aoc.PuzzleSet
+
+fun PuzzleSet.day20() = puzzle(20) {
+    // Dang that is a stupid algorithm
+    val num = input.toInt()
+    val n10 = num / 10
+    val list = MutableList(n10) { 0 }
+    fun solve() = list.withIndex().filter { it.value >= num }.minOf { it.index + 1 }.s()
+
+    (1..n10).forEach { i -> (i..n10 step i).forEach { list[it - 1] += i * 10 } }
+    partOne = solve()
+
+    list.replaceAll { 0 }
+    (1..n10).forEach { i -> (i..n10 step i).take(50).forEach { list[it - 1] += i * 11 } }
+    partTwo = solve()
+}
