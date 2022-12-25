0bcfece9b8cc8760edc1541a63d73515d94cfa3d
770grappenmaker <770grappenmaker@gmail.com>
2022-12-25 06:39:35 +0100

    Day 25 - merry christmas - 2022

---
+++ src/main/kotlin/com/grappenmaker/aoc/year22/Day25.kt
@@ -0,0 +1,35 @@
+package com.grappenmaker.aoc.year22
+
+import com.grappenmaker.aoc.PuzzleSet
+
+fun PuzzleSet.day25() = puzzle {
+    partOne = inputLines.sumOf { it.toSnafuLong() }.toSnafuString()
+    partTwo = "Merry Christmas! And Happy New Year!"
+}
+
+fun String.toSnafuLong() = deepen().asReversed().foldIndexed(0L) { idx, acc, curr ->
+    acc + when (curr) {
+        '-' -> -1
+        '=' -> -2
+        else -> curr.digitToInt().toLong()
+    } * fivePow(idx)
+}
+
+fun Long.toSnafuString() = buildString {
+    var curr = this@toSnafuString
+    while (curr != 0L) {
+        append(when(val v = curr % 5) {
+            4L -> '-'
+            3L -> '='
+            else -> v.toInt().digitToChar()
+        })
+
+        curr = (curr + 2) / 5
+    }
+}
+
+fun fivePow(n: Int): Long {
+    var result = 1L
+    repeat(n) { result *= 5 }
+    return result
+}
