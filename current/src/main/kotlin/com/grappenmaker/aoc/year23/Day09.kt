11551da4b07afafc38ff56c28ae2c8d8ab2d36f6
770grappenmaker <770grappenmaker@gmail.com>
2023-12-09 07:17:28 +0100

    Day 9

---
+++ current/src/main/kotlin/com/grappenmaker/aoc/year23/Day09.kt
@@ -0,0 +1,13 @@
+package com.grappenmaker.aoc.year23
+
+import com.grappenmaker.aoc.*
+
+fun PuzzleSet.day9() = puzzle(day = 9) {
+    val seq = inputLines.map { ns ->
+        generateSequence(ns.split(" ").map(String::toInt)) { c -> c.windowed(2) { (a, b) -> b - a } }
+            .takeUntil { it.any { c -> c != 0 } }.toList()
+    }
+
+    partOne = seq.sumOf { s -> s.sumOf { it.last() } }.s()
+    partTwo = seq.sumOf { s -> s.foldRight(0L) { c, a -> c.first() - a } }.s()
+}
