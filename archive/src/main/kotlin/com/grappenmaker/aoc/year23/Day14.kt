069f17af6a4b54653fa399fe061d4836a12c4101
770grappenmaker <770grappenmaker@gmail.com>
2023-12-14 07:24:52 +0100

    Day 14 'optimized'

--- archive/src/main/kotlin/com/grappenmaker/aoc/year23/Day14.kt
+++ archive/src/main/kotlin/com/grappenmaker/aoc/year23/Day14.kt
@@ -5,20 +5,44 @@ import com.grappenmaker.aoc.Direction.*
 
 fun PuzzleSet.day14() = puzzle(day = 14) {
     val start = inputLines.asCharGrid()
+    val maze = start.mapElements { if (it == 'O') '.' else it }
 
-    // TODO: think about solving it by rolling just the set of points
-    fun Grid<Char>.step(d: Direction): Grid<Char> = asMutableGrid().apply {
-        findPointsValued('O').asSequence().filter { it + d in this && this[it + d] == '.' }.forEach { p ->
-            this[p + d] = 'O'
-            this[p] = '.'
+    // optimized solution
+    val ps = start.findPointsValued('O').toSet()
+
+    fun Set<Point>.isEnd(p: Point, d: Direction) = p + d !in maze || maze[p + d] != '.' || p + d in this
+    fun Set<Point>.step(d: Direction): Set<Point> {
+        val res = toHashSet()
+
+        for (p in this) {
+            if (isEnd(p, d)) continue
+
+            res -= p
+            res += p + d
         }
-    }.asGrid()
 
-    fun Grid<Char>.solve() = findPointsValued('O').sumOf { height - it.y }.s()
-    fun Grid<Char>.roll(d: Direction) = generateSequence(this) { it.step(d) }.first { g ->
-        g.findPointsValued('O').all { it + d !in g || g[it + d] != '.' }
+        return res
     }
 
-    partOne = start.roll(UP).solve()
-    partTwo = start.patternRepeating(1000000000) { it.roll(UP).roll(LEFT).roll(DOWN).roll(RIGHT) }.solve()
+    fun Set<Point>.solve() = sumOf { start.height - it.y }.s()
+    fun Set<Point>.roll(d: Direction) = generateSequence(this) { it.step(d) }.first { g -> g.all { g.isEnd(it, d) } }
+
+    partOne = ps.roll(UP).solve()
+    partTwo = ps.patternRepeating(1000000000) { it.roll(UP).roll(LEFT).roll(DOWN).roll(RIGHT) }.solve()
+
+    // sane solution
+//    fun Grid<Char>.step(d: Direction): Grid<Char> = asMutableGrid().apply {
+//        findPointsValued('O').asSequence().filter { it + d in this && this[it + d] == '.' }.forEach { p ->
+//            this[p + d] = 'O'
+//            this[p] = '.'
+//        }
+//    }.asGrid()
+//
+//    fun Grid<Char>.solve() = findPointsValued('O').sumOf { height - it.y }.s()
+//    fun Grid<Char>.roll(d: Direction) = generateSequence(this) { it.step(d) }.first { g ->
+//        g.findPointsValued('O').all { it + d !in g || g[it + d] != '.' }
+//    }
+//
+//    partOne = start.roll(UP).solve()
+//    partTwo = start.patternRepeating(1000000000) { it.roll(UP).roll(LEFT).roll(DOWN).roll(RIGHT) }.solve()
 }
