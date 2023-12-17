270d373a6b244930a1e4da73206c770fa525bfed
770grappenmaker <770grappenmaker@gmail.com>
2023-12-17 07:16:45 +0100

    Day 17 cleanup

--- archive/src/main/kotlin/com/grappenmaker/aoc/year23/Day17.kt
+++ archive/src/main/kotlin/com/grappenmaker/aoc/year23/Day17.kt
@@ -3,20 +3,24 @@ package com.grappenmaker.aoc.year23
 import com.grappenmaker.aoc.*
 
 fun PuzzleSet.day17() = puzzle(day = 17) {
-    val g = inputLines.asDigitGrid()
-
     data class State(
         val pos: Point = Point(0, 0),
         val sameDir: Int = 0,
         val lastDir: Direction? = null,
     )
 
+    val g = inputLines.asDigitGrid()
     val t = g.bottomRightCorner
     fun solve(
+        n: Int,
         cond: (State) -> Boolean = { true },
-        dirs: (State) -> List<Direction>,
     ) = dijkstra(initial = State(), isEnd = { it.pos == t && cond(it) }, neighbors = { s ->
-        dirs(s).filter { s.pos + it in g }.map {
+        when {
+            s.lastDir == null -> Direction.entries
+            s.sameDir == n -> listOf(s.lastDir.next(1), s.lastDir.next(-1))
+            cond(s) -> Direction.entries - setOf(-s.lastDir)
+            else -> listOf(s.lastDir)
+        }.filter { s.pos + it in g }.map {
             State(
                 pos = s.pos + it,
                 sameDir = if (it == s.lastDir) s.sameDir + 1 else 1,
@@ -25,21 +29,7 @@ fun PuzzleSet.day17() = puzzle(day = 17) {
         }
     }, findCost = { g[it.pos] })!!.cost.s()
 
-    partOne = solve {
-        when {
-            it.lastDir == null -> Direction.entries
-            it.sameDir == 3 -> listOf(it.lastDir.next(1), it.lastDir.next(-1))
-            else -> Direction.entries - setOf(-it.lastDir)
-        }
-    }
-
     // advent of reading comprehension "(or even before it can stop at the end)"
-    partTwo = solve(cond = { it.sameDir >= 4 }) {
-        when {
-            it.lastDir == null -> Direction.entries
-            it.sameDir == 10 -> listOf(it.lastDir.next(1), it.lastDir.next(-1))
-            it.sameDir >= 4 -> Direction.entries - setOf(-it.lastDir)
-            else -> listOf(it.lastDir)
-        }
-    }
+    partOne = solve(3)
+    partTwo = solve(10) { it.sameDir >= 4 }
 }
