da8c2236ded1178f80bc8d0891b8e7a5e1dab519
770grappenmaker <770grappenmaker@gmail.com>
2023-12-22 06:59:33 +0100

    Day 22 (optimized)

--- archive/src/main/kotlin/com/grappenmaker/aoc/year23/Day22.kt
+++ archive/src/main/kotlin/com/grappenmaker/aoc/year23/Day22.kt
@@ -3,37 +3,25 @@ package com.grappenmaker.aoc.year23
 import com.grappenmaker.aoc.*
 
 fun PuzzleSet.day22() = puzzle(day = 22) {
-    val bricks = inputLines.map { l ->
-        l.split("~").map { p ->
+    val bp = inputLines.map { l ->
+        val (a, b) = l.split("~").map { p ->
             val (a, b, c) = p.split(",").map(String::toInt)
             Point3D(a, b, c)
-        }.asPair()
-    }
-
-    val bp = bricks.map { (a, b) ->
-        val ps = hashSetOf<Point3D>()
-
-        for (x in a.x..b.x) {
-            for (y in a.y..b.y) {
-                for (z in a.z..b.z) {
-                    ps += Point3D(x, y, z)
-                }
-            }
         }
 
-        ps
-    }
+        (a..b).points
+    }.sortedBy { b -> b.minOf { it.z } }
 
-    fun List<Set<Point3D>>.step(): Pair<List<Set<Point3D>>, Int> {
-        val new = mutableListOf<Set<Point3D>>()
+    fun List<List<Point3D>>.step(): Pair<List<List<Point3D>>, Int> {
+        val new = mutableListOf<List<Point3D>>()
         val fallen = hashSetOf<Point3D>()
         var a = 0
 
-        for (b in this.sortedBy { b -> b.minOf { it.z } }) {
+        for (b in this) {
             var cb = b
             while (true) {
-                val down = cb.mapToSet { p -> p.mapZ { it - 1 } }
-                if (down.any { it in fallen || it.z <= 0 }) {
+                val down = cb.map { p -> p.mapZ { it - 1 } }
+                if (down.any { it.z <= 0 || it in fallen }) {
                     new += cb
                     fallen += cb
                     if (cb != b) a++
@@ -48,11 +36,7 @@ fun PuzzleSet.day22() = puzzle(day = 22) {
     }
 
     val (s) = bp.step()
-    val t = s.map { b ->
-        val n = s.minusElement(b)
-        n.step().second
-    }
-
+    val t = s.map { b -> s.minusElement(b).step().second }
     partOne = t.countContains(0).s()
     partTwo = t.sum().s()
 }
