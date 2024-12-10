3e1cbf7ee1572c8090d86981adab6e4168af5027
770grappenmaker <770grappenmaker@gmail.com>
2024-12-10 06:14:59 +0100

    Day 10

--- current/src/main/kotlin/com/grappenmaker/aoc/year24/Day10.kt
+++ current/src/main/kotlin/com/grappenmaker/aoc/year24/Day10.kt
@@ -4,22 +4,40 @@ package com.grappenmaker.aoc.year24
 
 import com.grappenmaker.aoc.*
 import kotlin.math.*
-import java.util.PriorityQueue
 import com.grappenmaker.aoc.Direction.*
 
-// remember that input is trimmed
 fun PuzzleSet.day10() = puzzle(day = 10) {
-    // test input overwrite
-//    val inputLines = """
-//
-//    """.trimIndent().lines()
-//    val input = inputLines.joinToString("\n")
+    val g = inputLines.asDigitGrid()
+    val th = g.findPointsValued(0)
 
-    inputLines
+    fun <T> solve(initial: (Point) -> T, gen: (T, Point) -> T, extr: (T) -> Point) = th.sumOf { p ->
+        val queue = ArrayDeque<T>()
+        queue += initial(p)
+        val seen = hashSetOf(queue.first())
+        var ans = 0
 
-//    partOne = launchVM(inputLines).also { it.run() }.registers[0]
-//    partOne = inputLines.asCharGrid()
-//    partOne = inputLines.asDigitGrid()
-//    partOne = inputLines.asGrid { it == '#' }
-//    partOne = input.doubleLines()
+        while (queue.isNotEmpty()) {
+            val curr = queue.removeLast()
+            val pt = extr(curr)
+
+            if (g[pt] == 9) {
+                ans++
+                continue
+            }
+
+            with (g) {
+                pt.adjacentSides()
+                    .filter { (g[it] - g[pt]) == 1 }
+                    .map { gen(curr, it) }
+                    .forEach { if (seen.add(it)) queue.addFirst(it) }
+            }
+        }
+
+        ans
+    }
+
+    partOne = solve(initial = { it }, gen = { _, p -> p }, extr = { it })
+
+    data class Node(val curr: Point, val path: Set<Point>)
+    partTwo = solve(initial = { Node(it, setOf(it)) }, gen = { curr, p -> Node(p, curr.path + p) }, extr = { it.curr })
 }
