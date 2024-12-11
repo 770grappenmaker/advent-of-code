2b7b7ab778a41455d8b155e7f5f58b336e58357a
770grappenmaker <770grappenmaker@gmail.com>
2024-12-11 06:22:07 +0100

    Day 11

---
+++ archive/src/main/kotlin/com/grappenmaker/aoc/year24/Day11.kt
@@ -0,0 +1,36 @@
+@file:Suppress("UnusedImport")
+
+package com.grappenmaker.aoc.year24
+
+import com.grappenmaker.aoc.*
+import kotlin.math.*
+import com.grappenmaker.aoc.Direction.*
+
+fun PuzzleSet.day11() = puzzle(day = 11) {
+    val initial = input.split(' ').map { it.toLong() }.frequencies().mapValues { it.value.toLong() }
+
+    fun Map<Long, Long>.update(): Map<Long, Long> = buildMap {
+        fun add(key: Long, value: Long) {
+            this[key] = getOrDefault(key, 0L) + value
+        }
+
+        for ((k, v) in this@update) {
+            val s = k.toString()
+
+            when {
+                k == 0L -> add(1, v)
+                s.length % 2 == 0 -> {
+                    val (a, b) = s.chunked(s.length / 2)
+                    add(a.toLong(), v)
+                    add(b.toLong(), v)
+                }
+
+                else -> add(k * 2024L, v)
+            }
+        }
+    }
+
+    fun solve(n: Int) = generateSequence(initial) { it.update() }.nth(n).values.sum()
+    partOne = solve(25)
+    partTwo = solve(75)
+}
