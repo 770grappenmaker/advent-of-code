2562fb9967019b85b4ed8d97440d1743186577b0
770grappenmaker <770grappenmaker@gmail.com>
2024-12-19 06:14:32 +0100

    Day 19

--- current/src/main/kotlin/com/grappenmaker/aoc/year24/Day19.kt
+++ current/src/main/kotlin/com/grappenmaker/aoc/year24/Day19.kt
@@ -11,17 +11,15 @@ fun PuzzleSet.day19() = puzzle(day = 19) {
     val (fp, sp) = input.doubleLines()
     val poss = fp.split(", ")
 
-    fun solve(cast: (Long) -> Long): Long {
-        val memo = hashMapOf<String, Long>()
-        return sp.lines().sumOf { l ->
-            fun test(left: String): Long = if (left.isEmpty()) 1 else memo.getOrPut(left) {
-                cast(poss.filter { test -> left.startsWith(test) }.sumOf { test(left.drop(it.length)) })
-            }
+    val memo = hashMapOf<String, Long>()
+    fun solve(cast: (Long) -> Long) = sp.lines().sumOf { l ->
+        fun test(left: String): Long = if (left.isEmpty()) 1 else cast(memo.getOrPut(left) {
+            poss.filter { test -> left.startsWith(test) }.sumOf { test(left.drop(it.length)) }
+        })
 
-            test(l)
-        }
+        test(l)
     }
 
-    partOne = solve { if (it > 0) 1 else 0 }
     partTwo = solve { it }
+    partOne = solve { if (it > 0) 1 else 0 }
 }
