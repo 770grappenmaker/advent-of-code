cee98b2a724f9101e29597a3d189af2c3265e2a1
770grappenmaker <770grappenmaker@gmail.com>
2025-12-07 06:22:51 +0100

    Day 7 2025

---
+++ archive/src/main/kotlin/com/grappenmaker/aoc/year25/Day07.kt
@@ -0,0 +1,23 @@
+package com.grappenmaker.aoc.year25
+
+import com.grappenmaker.aoc.*
+
+fun main() = simplePuzzle(7, 2025) {
+    var p1 = 0
+    val g = inputLines.asCharGrid()
+    val s = g.findPointsValued('S').single()
+    val memo = hashMapOf<Point, Long>()
+
+    fun recur(head: Point): Long = memo.getOrPut(head) {
+        val d = head + Direction.DOWN
+        if (d !in g) return 1
+
+        if (g[d] == '^') {
+            p1++
+            recur(d + Direction.LEFT) + recur(d + Direction.RIGHT)
+        } else recur(d)
+    }
+
+    partTwo = recur(s)
+    partOne = p1
+}
